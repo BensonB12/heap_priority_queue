7dea6f62c45096acab06d6fc786a4a56511bd80f
Benson Bird <bensonbird2@gmail.com>
2024-09-30 14:51:03 -0600

    DONES

--- test_heap.py
+++ test_heap.py
@@ -4,38 +4,42 @@ from heap import Heap
 
 def test_put_get():
     heap = Heap()
-    heap.put(Subject(1, "a", 5))
-    heap.put(Subject(2, "b", 2))
+    heap.put(Subject(key=1, value="a", priority=5))
+    heap.put(Subject(key=2, value="b", priority=2))
     assert heap.get(1).value == "a"
     assert heap.get(2).priority == 2
 
 
 def test_get_next():
     heap = Heap()
-    heap.put(Subject(1, "a", 5))
-    heap.put(Subject(2, "b", 2))
+    heap.put(Subject(key=1, value="a", priority=5))
+    heap.put(Subject(key=2, value="b", priority=2))
     assert heap.get_next().value == "b"  # Min priority at the root
 
 
 def test_remove():
     heap = Heap()
-    heap.put(Subject(1, "a", 5))
-    heap.put(Subject(2, "b", 2))
+    heap.put(Subject(key=1, value="a", priority=5))
+    heap.put(Subject(key=2, value="b", priority=2))
     heap.remove(2)
     assert heap.get_next().key == 1
 
 
 def test_initialize():
     heap = Heap()
-    entries = [Subject(1, "a", 5), Subject(2, "b", 2), Subject(3, "c", 3)]
+    entries = [
+        Subject(key=1, value="a", priority=5),
+        Subject(key=2, value="b", priority=2),
+        Subject(key=3, value="c", priority=3),
+    ]
     heap.initialize(entries)
     assert heap.get_next().key == 2  # Min priority at the root
 
 
 def test_get_all():
     heap = Heap()
-    heap.put(Subject(1, "a", 5))
-    heap.put(Subject(2, "b", 2))
-    heap.put(Subject(3, "c", 3))
+    heap.put(Subject(key=1, value="a", priority=5))
+    heap.put(Subject(key=2, value="b", priority=2))
+    heap.put(Subject(key=3, value="c", priority=3))
     all_entries = heap.get_all()
     assert len(all_entries) == 3
